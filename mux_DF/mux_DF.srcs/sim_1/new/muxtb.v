1824cf4ce2de84b585f26be052ceb8dad73be36c
bxbx1205 <srbhanarkar05@gmail.com>
2025-02-04 11:10:29 +0530

    modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/compile.bat 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/compile.log 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/elaborate.bat 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/muxi_DF_tb_behav.wdb 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/simulate.bat 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/obj/xsim_0.win64.obj 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.dbg 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.mem 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.reloc 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.rlx 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.rtti 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsim.xdbg 	new file:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsimSettings.ini 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsimk.exe 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/muxi_DF_tb_behav/xsimkernel.log 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/muxi_@d@f.sdb 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/muxi_@d@f_tb.sdb 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/xil_defaultlib.rlx 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xvlog.log 	modified:   mux_DF/mux_DF.sim/sim_1/behav/xsim/xvlog.pb 	modified:   mux_DF/mux_DF.srcs/sim_1/new/muxtb.v 	modified:   mux_DF/mux_DF.xpr

--- mux_DF/mux_DF.srcs/sim_1/new/muxtb.v
+++ mux_DF/mux_DF.srcs/sim_1/new/muxtb.v
@@ -1,3 +1,27 @@
+//`timescale 1ns / 1ps
+
+//module muxi_DF_tb;
+
+//    reg [3:0] i;
+//    reg [1:0] s;
+//    wire y;
+    
+//    muxi_DF uut (i,s,y);
+    
+//    initial 
+//    begin 
+    
+
+    
+//        i = 4'b1010;
+//        s = 2'b00; #10;
+//        s = 2'b01; #10;
+//        s = 2'b10; #10;
+//        s = 2'b11; #10;
+//        $stop;
+//    end
+
+//endmodule
 `timescale 1ns / 1ps
 
 module muxi_DF_tb;
@@ -6,16 +30,19 @@ module muxi_DF_tb;
     reg [1:0] s;
     wire y;
     
-    muxi_DF uut (i,s,y);
-    
-    initial 
-    begin
-        i = 4'b1010;
-        s = 2'b00; #10;
-        s = 2'b01; #10;
-        s = 2'b10; #10;
-        s = 2'b11; #10;
-        $stop;
+    muxi_DF uut (i, s, y);  
+
+    initial begin
+        i = 4'b0000;
+        s = 2'b00;
+
+        repeat (4) begin  
+            #10;
+            s = s + 1;  
+        end  
+
+        $stop; 
     end
 
 endmodule
+
